f57f14a88ac149c8ebe14de8dd414057eb3b5757
mestrada <mestrada@zitrointeractive.com>
2023-03-20 11:32:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import MovieCard from "./MovieCard";
+import SearchIcon from './search.svg';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=bde87b6a';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&S=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies(searchTitle);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTitle}
+          onChange={(e) => setSearchTitle(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTitle)}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              {movies.map((movie) => (
+                <MovieCard movie={movie}/>
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
